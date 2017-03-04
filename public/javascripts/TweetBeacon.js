57b0b2aebe767f5920026a681a023570b190328e
GIANNIS-AGGELIS <p15ange@ionio.gr>
2017-03-04 18:38:56 +0200

    Update TweetBeacon.js

--- public/javascripts/TweetBeacon.js
+++ public/javascripts/TweetBeacon.js
@@ -17,21 +17,29 @@ TweetBeacon = function(tweet) {
   // Set base color depending on sentiment score
   this.color = 0xFFFFFF;
 
-  if (tweet.sentiment.score >= 0.70) 
+  if (tweet.sentiment.score >= 0.80) 
   {
     this.color = 0x0000FF;
   }
-  else if (tweet.sentiment.score < 0.70 && tweet.sentiment.score >= 0.35)
+  else if (tweet.sentiment.score < 0.80 && tweet.sentiment.score >= 0.40)
   {
     this.color = 0x008000;
   }
-  else if (tweet.sentiment.score < 0.35 && tweet.sentiment.score >= 0)
+  else if (tweet.sentiment.score < 0.40 && tweet.sentiment.score >= 0)
   {
     this.color = 0xffff00;
   }
-  else if (tweet.sentiment.score < 0) 
+  else if (tweet.sentiment.score < 0  && tweet.sentiment.score >= -0.30 ) 
+  {
+    this.color = 0x808080;
+  }
+  else if (tweet.sentiment.score < -0,30 && tweet.sentiment.score > -0.80) 
   {
     this.color = 0xFF0000;
+  }
+  else if (tweet.sentiment.score >= -0.80)
+  {
+    this.color = 0x990000;
   }
 
   this.addBeam();
