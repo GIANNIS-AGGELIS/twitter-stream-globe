d0a0eab54090951b76d048291916277f54b6010d
GIANNIS-AGGELIS <p15ange@ionio.gr>
2017-03-22 00:18:52 +0200

    Update TwitterStreamGlobe.js

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -163,7 +163,7 @@
 	 */ 
 	function render () {
 
-		earthMesh.rotation.y = earthMesh.rotation.y + 0.035;
+		earthMesh.rotation.y = earthMesh.rotation.y - 0.035;
 		
 	  renderer.autoClear = false;
 	  renderer.clear();
